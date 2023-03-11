10f159a6aeac6e51f861230c3f1b74fd90a835b6
whs <hswongac@gmail.com>
2023-03-11 01:45:32 -0500

    Add cdk

---
+++ cdk-question-1/lib/cdk-question-1-stack.ts
@@ -0,0 +1,66 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as autoscaling from "aws-cdk-lib/aws-autoscaling";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as elbv2 from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
+
+export class CdkQuestion1Stack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'CdkQuestion1Queue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+
+    const vpc = new ec2.Vpc(this, "Vpc", {
+      ipAddresses: ec2.IpAddresses.cidr("10.0.0.0/16"),
+    });
+
+    const alb = new elbv2.ApplicationLoadBalancer(this, "Alb", {
+      vpc,
+      internetFacing: true,
+    });
+
+    const sg = new ec2.SecurityGroup(this, "Sg", { vpc });
+
+    const asg = new autoscaling.AutoScalingGroup(this, "Asg", {
+      vpc,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T2,
+        ec2.InstanceSize.MICRO
+      ),
+      machineImage: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      }),
+      securityGroup: sg,
+      maxCapacity: 10,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      keyName: "test",
+    });
+
+    const listener = alb.addListener("Listener", {
+      port: 80,
+    });
+
+    listener.addTargets("Targets", {
+      port: 80,
+      targets: [asg],
+    });
+
+    new cloudfront.Distribution(this, "Distribution", {
+      defaultBehavior: {
+        origin: new origins.LoadBalancerV2Origin(alb, {
+          protocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,
+        }),
+      },
+    });
+  }
+}
---
+++ cdk-question-1/test/cdk-question-1.test.ts
@@ -0,0 +1,16 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkQuestion1 from '../lib/cdk-question-1-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-question-1-stack.ts
+test("SQS Queue Created", () => {
+  //   const app = new cdk.App();
+  //     // WHEN
+  //   const stack = new CdkQuestion1.CdkQuestion1Stack(app, 'MyTestStack');
+  //     // THEN
+  //   const template = Template.fromStack(stack);
+  //   template.hasResourceProperties('AWS::SQS::Queue', {
+  //     VisibilityTimeout: 300
+  //   });
+});
