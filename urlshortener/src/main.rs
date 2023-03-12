05f5bf26da7da7363a6cc84728729375cb160975
whs <hswongac@gmail.com>
2023-03-11 19:06:34 -0500

    Add base code

--- urlshortener/src/main.rs
+++ urlshortener/src/main.rs
@@ -1,15 +1,49 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 mod short_id;
 
+use rocket::response::{status::NotFound, Redirect};
+use rocket::serde::{Serialize, json::Json, Deserialize};
 use short_id::ShortId;
 
+#[derive(Responder)]
+enum MyResponse {
+    Redirect(Redirect),
+    NotFound(NotFound<String>),
+}
+
 #[get("/<id>")]
-async fn retrieve(id: ShortId<'_>) -> &'static str {
-    "Test"
+async fn retrieve(id: ShortId<'_>) -> MyResponse {
+    let url = id.get_url().await.unwrap_or("".to_string());
+    if url == "" {
+        return MyResponse::NotFound(NotFound("".to_string()));
+    } else {
+        return MyResponse::Redirect(Redirect::to(url));
+    }
+}
+
+#[derive(Deserialize)]
+struct Url {
+    url: String,
+}
+
+#[derive(Serialize)]
+struct ResponseUrl {
+    url: String,
+    shortenUrl: String,
+}
+
+#[post("/newurl", data = "<u>")]
+async fn create(u: Json<Url>) -> Json<ResponseUrl> {
+    let url = u.url.clone();
+
+    let id = ShortId::new().set_url(&url).await;
+
+    Json(ResponseUrl{url: url, shortenUrl: id.unwrap()})
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![retrieve])
+    rocket::build().mount("/", routes![create, retrieve])
 }
--- urlshortener/src/short_id.rs
+++ urlshortener/src/short_id.rs
@@ -1,8 +1,9 @@
-use std::borrow::Cow;
-
+use aws_config::meta::region::RegionProviderChain;
+use aws_sdk_dynamodb::model::AttributeValue;
+use aws_sdk_dynamodb::{Client, Error};
 use rand::{self, Rng};
-
 use rocket::request::FromParam;
+use std::borrow::Cow;
 
 pub struct ShortId<'a>(Cow<'a, str>);
 
@@ -18,6 +19,48 @@ impl ShortId<'_> {
 
         ShortId(Cow::Owned(id))
     }
+
+    pub async fn set_url(&self, url: &str) -> Result<String, Error> {
+        let region_provider = RegionProviderChain::default_provider().or_else("us-east-1");
+        let config = aws_config::from_env().region(region_provider).load().await;
+
+        let client = Client::new(&config);
+
+        let shortid_av = AttributeValue::S(self.0.to_string());
+        let url_av = AttributeValue::S(url.to_string());
+
+        client
+            .put_item()
+            .table_name("test")
+            .item("shortid", shortid_av)
+            .item("url", url_av)
+            .send()
+            .await?;
+
+        Ok(self.0.to_string())
+    }
+
+    pub async fn get_url(&self) -> Result<String, Error> {
+        let region_provider = RegionProviderChain::default_provider().or_else("us-east-1");
+        let config = aws_config::from_env().region(region_provider).load().await;
+
+        let client = Client::new(&config);
+
+        let items = client
+            .query()
+            .table_name("test")
+            .key_condition_expression("shortid = :shortid")
+            .expression_attribute_values(":shortid", AttributeValue::S(self.0.to_string()))
+            .send()
+            .await?
+            .items
+            .unwrap();
+
+        match items.first() {
+            Some(m) => Ok(m.get("url").unwrap().as_s().unwrap().clone()),
+            _ => Ok("".to_string()),
+        }
+    }
 }
 
 impl<'a> FromParam<'a> for ShortId<'a> {
@@ -28,7 +71,9 @@ impl<'a> FromParam<'a> for ShortId<'a> {
             return Err(param);
         }
 
-        param.chars().all(|c| c.is_ascii_alphanumeric())
+        param
+            .chars()
+            .all(|c| c.is_ascii_alphanumeric())
             .then(|| ShortId(param.into()))
             .ok_or(param)
     }
